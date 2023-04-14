7091b9f9299d36a693db7f66e31c78731c54e652
Atharva Deosthale <atharva.deosthale17@gmail.com>
2023-04-15 01:12:34 +0530

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.11",
     settings: {
       optimizer: {
         enabled: true,
